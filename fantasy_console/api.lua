482ef90dda19c5235e0cae376dc92dc166b6f62f
Tomasz Nowakowski <toalak@gmail.com>
2024-05-06 19:18:28 +0200

    Using global functions as a way to expose the api to users without forcing them to write module name all the time

---
+++ fantasy_console/api.lua
@@ -0,0 +1,3 @@
+function Write(s, x, y)
+    love.graphics.print(s, x, y, 0, 1, 1, 0, 0)
+end
--- fantasy_console/main.lua
+++ fantasy_console/main.lua
@@ -1,3 +1,6 @@
+require "api"
+
 function love.draw()
-    love.graphics.print("Hello world!")
+    --love.graphics.print("Hello world!")
+    Write("Hello world!", 10, 20)
 end
