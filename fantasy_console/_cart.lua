292a0967c6156c1b0b4cb587bb0d922638936075
Tomasz Nowakowski <toalak@gmail.com>
2024-05-06 22:31:46 +0200

    Test 1 - passed

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -4,6 +4,7 @@ function Init()
     txt = "Hello from Love2D!"
     num = 1
     txt2 = "Hello from Fantasy Console! x" .. num
+    txt3 = Sub(txt2, 1, 5)  -- "Hello" expected
 end
 
 function Update()
@@ -14,4 +15,5 @@ end
 function Draw()
     Write(txt, 10, 10)
     Write(txt2, 10, 50)
+    Write(txt3, 10, 90)
 end
