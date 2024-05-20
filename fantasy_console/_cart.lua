2a180e0983cec8b5dfaec1a538086eea6f775ce5
Tomasz Nowakowski <toalak@gmail.com>
2024-05-20 16:28:19 +0200

    Test

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -7,7 +7,7 @@ function Init()
     txt3 = Sub("Česká", 2, 5) -- eská expected
     local gamepixel = require "gamepixel"
     local palette = require "palette"
-    pix = gamepixel.new_gamepixel(1, 2, palette.red_bold)
+    pix = gamepixel.new_gamepixel(1, 2, palette.green)
     pix2 = gamepixel.new_gamepixel(2, 2, palette.green_bold)
 end
 
@@ -22,14 +22,14 @@ function Draw()
     Write(txt3, 1, 1+16)
     --Ppset(pix)
     local palette = require "palette"
-    Pset(1, 1, palette.blue_bold)
-    Pset(2, 1, palette.magenta_bold)
+    Pset(1, 1, BlueBold())
+    Pset(2, 1, PinkBold())
     Ppset(pix)
     Ppset(pix2)
-    Pset(29, 30, palette.cyan_bold)
-    Pset(29, 34, palette.cyan)
-    Rect(29, 30, 4, 4, palette.green_bold)
-    Pset(34, 30, palette.cyan)
-    Pset(34, 34, palette.cyan_bold)
-    Rectfill(34, 30, 4, 4, palette.blue_bold)
+    Pset(29, 30, CyanBold())
+    Pset(29, 34, Cyan())
+    Rect(29, 30, 4, 4, GreenBold())
+    Pset(34, 30, Cyan())
+    Pset(34, 34, CyanBold())
+    Rectfill(34, 30, 4, 4, BlueBold())
 end
