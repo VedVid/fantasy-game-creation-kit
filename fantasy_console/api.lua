12e996c802144812f472100ddea8d175313206d4
Tomasz Nowakowski <toalak@gmail.com>
2024-05-20 23:49:59 +0200

    Variable names changed

--- fantasy_console/api.lua
+++ fantasy_console/api.lua
@@ -380,51 +380,51 @@ function Circfill(x, y, r, color)
 end
 
 
-function Oval(xc, yc, rx, ry, color)
-	local dx, dy, d1, d2, x, y
-	x = 0;
-	y = ry;
+function Oval(x, y, rx, ry, color)
+	local dx, dy, d1, d2
+	local xx = 0;
+	local yy = ry;
 	
 	d1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx)
-	dx = 2 * ry * ry * x
-	dy = 2 * rx * rx * y
+	dx = 2 * ry * ry * xx
+	dy = 2 * rx * rx * yy
 	
 	while (dx < dy) do
 		
-        Pset(x + xc, y + yc, color)
-        Pset(-x + xc, y + yc, color)
-        Pset(x + xc, -y + yc, color)
-        Pset(-x + xc, -y + yc, color)
+        Pset(xx + x, yy + y, color)
+        Pset(-xx + x, yy + y, color)
+        Pset(xx + x, -yy + y, color)
+        Pset(-xx + x, -yy + y, color)
 
 		if d1 < 0 then
-			x = x + 1
+			xx = xx + 1
 			dx = dx + (2 * ry * ry)
 			d1 = d1 + dx + (ry * ry)
 		else
-			x = x + 1
-			y = y - 1
+			xx = xx + 1
+			yy = yy - 1
 			dx = dx + (2 * ry * ry)
 			dy = dy - (2 * rx * rx)
 			d1 = d1 + dx - dy + (ry * ry)
 		end
 	end
 	
-	d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) + ((rx * rx) * ((y - 1) * (y - 1))) - (rx * rx * ry * ry)
+	d2 = ((ry * ry) * ((xx + 0.5) * (xx + 0.5))) + ((rx * rx) * ((yy - 1) * (yy - 1))) - (rx * rx * ry * ry)
 	
-	while (y >= 0) do
+	while (yy >= 0) do
 		
-        Pset(x + xc, y + yc, color)
-        Pset(-x + xc, y + yc, color)
-        Pset(x + xc, -y + yc, color)
-        Pset(-x + xc, -y + yc, color)
+        Pset(xx + x, yy + y, color)
+        Pset(-xx + x, yy + y, color)
+        Pset(xx + x, -yy + y, color)
+        Pset(-xx + x, -yy + y, color)
 		
 		if d2 > 0 then
-			y = y - 1
+			yy = yy - 1
 			dy = dy - (2 * rx * rx)
 			d2 = d2 + (rx * rx) - dy
 		else
-			y = y - 1
-			x = x + 1
+			yy = yy - 1
+			xx = xx + 1
 			dx = dx + (2 * ry * ry)
 			dy = dy - (2 * rx * rx)
 			d2 = d2 + dx - dy + (rx * rx)
