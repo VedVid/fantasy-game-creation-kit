ac44feac281187eda3b09d3da8c891df48a77980
Tomasz Nowakowski <toalak@gmail.com>
2024-06-05 21:24:56 +0200

    First prototype of snake – from Eric Yuzwa on YT

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -1,46 +1,143 @@
 require "api"
 
-local cx, cy
-local rs
-local s
+local x = {}
+local y = {}
+local x_dir = 0
+local y_dir = 0
+local score = 0
+local tile_num = 32
+local tile_size = math.floor(192/tile_num)
+local fruit = {}
+fruit.x = 0
+fruit.y = 0
+fruit.color=PinkBold
 
 function Init()
-    cx, cy = 1, 1
-    rs = 4
-    s = "Full Fledged String"
-    s = Sub(s, 6, 12)
+    x[1] = math.floor(tile_size * (tile_num / 4))
+    y[1] = math.floor(tile_size * (tile_num / 2))
+    x_dir = 1
+    for i = 2, 7 do
+        x[i] = ((x[i-1] / tile_size) - 1) * tile_size
+        y[i] = y[1]
+    end
+    Update_fruit()
 end
 
 function Input()
-    if Btnp("z") then
-        rs = rs + 1
-    elseif Btnp("x") then
-        rs = rs - 1
-        if rs <= 1 then rs = 2 end
-    elseif Btn("left") then
-        cx = cx - 1
-        if cx < 1 then cx = 1 end
+    if Btn("left") then
+        x_dir = -1
+        y_dir = 0
     elseif Btn("right") then
-        cx = cx + 1
+        x_dir = 1
+        y_dir = 0
     elseif Btn("up") then
-        cy = cy - 1
-        if cy < 1 then cy = 1 end
+        x_dir = 0
+        y_dir = -1
     elseif Btn("down") then
-        cy = cy + 1
+        x_dir = 0
+        y_dir = 1
     end
 end
 
 function Update()
-    do end
+    if (is_wall_collision()) then
+        return
+    end
+    local collide = false
+    collide = is_fruit_collision()
+    if collide then
+        score = score + 10
+        for i=#x+1, 2, -1 do
+            x[i] = x[i-1]
+            y[i] = y[i-1]
+        end
+        x[1] = fruit.x
+        y[1] = fruit.y
+        Update_fruit()
+    else
+        Update_snake()
+    end
 end
 
 function Draw()
     Cls()
-    Rect(cx, cy, rs, rs, BlueBold)
-    Write("Hello, Worldy!", 4, 10)
-    Write("Valid: abcdefghijklmnopqrstuvwxyz", 4, 18)
-    Write("Valid: ABCDEFGHIJKLMNOPQRSTUVWXYZ", 4, 26)
-    Write("Valid:  !#$%&'()*+,-./0123456789:;<=>?", 4, 34)
-    Write('Valid: "@[\\]^_`{|}~', 4, 42)
-    Write(s, 4, 50)
+    Draw_snake()
+    Draw_fruit()
+    Draw_score()
+end
+
+
+function Draw_fruit()
+    Rectfill(
+        fruit.x,
+        fruit.y,
+        fruit.x + tile_size - 1,
+        fruit.y + tile_size - 1,
+        fruit.color
+    )
+end
+
+
+function Draw_score()
+    Write("Score: " .. score, 1, 1, White)
+end
+
+
+function Draw_snake()
+    Rectfill(
+        x[1],
+        y[1],
+        x[1] + tile_size - 1,
+        y[1] + tile_size - 1,
+        Green
+    )
+
+    for i=2, #x do
+        Rectfill(
+            x[i],
+            y[i],
+            x[i] + tile_size - 1,
+            y[i] + tile_size - 1,
+            GreenBold
+        )
+    end
+end
+
+
+function Update_fruit()
+    fruit.x = math.floor(tile_num * tile_size)
+    fruit.y = math.floor(tile_num * tile_size)
+end
+
+
+function Update_snake()
+    local temp1x = x[1]
+    local temp1y = y[1]
+    local temp2x, temp2y
+    x[1] = math.floor(x[1] + (x_dir * tile_size))
+    y[1] = math.floor(y[1] + (y_dir * tile_size))
+    for i=2, #x do
+        temp2x = x[i]
+        temp2y = y[i]
+        x[i] = temp1x
+        y[i] = temp1y
+        temp1x = temp2x
+        temp1y = temp2y
+    end
+end
+
+
+function is_fruit_collision()
+    if(x[1] + (x_dir * tile_size) == fruit.x and y[1] + (y_dir * tile_size) == fruit.y) then
+        return true
+    end
+    return false
+end
+
+
+function is_wall_collision()
+    if(x[1] > 192 or x[1] < 0 or y[1] < 0 or y[1] > 192) then
+        return true
+    end
+    return false
 end
