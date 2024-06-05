4e62d427f6dff0e1a9f927ee32b8b0aaa913a0fc
Tomasz Nowakowski <toalak@gmail.com>
2024-06-05 23:19:04 +0200

    Snake coords are arrays now

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -3,8 +3,8 @@ require "api"
 local score = 0
 local x_dir = 0
 local y_dir = 0
-local snake_x = 0
-local snake_y = 0
+local snake_x = {}
+local snake_y = {}
 local fruit_x = 0
 local fruit_y = 0
 local map_x_min = 0
@@ -16,8 +16,8 @@ local game_over = false
 function Init()
     x_dir = 1
     Update_fruit()
-    snake_x = 8 * 8
-    snake_y = 8 * 8
+    snake_x[1] = 8 * 8
+    snake_y[1] = 8 * 8
 end
 
 function Input()
@@ -71,22 +71,22 @@ end
 
 
 function Draw_snake()
-    Rectfill(snake_x, snake_y, 8, 8, Green)
+    Rectfill(snake_x[1], snake_y[1], 8, 8, Green)
 end
 
 
 function Update_snake()
-    snake_x = snake_x + (x_dir * 8)
-    snake_y = snake_y + (y_dir * 8)
-    if snake_x <= map_x_min or snake_x >= map_x_max - 8 then
+    snake_x[1] = snake_x[1] + (x_dir * 8)
+    snake_y[1] = snake_y[1] + (y_dir * 8)
+    if snake_x[1] <= map_x_min or snake_x[1] >= map_x_max - 8 then
         game_over = true
         return
     end
-    if snake_y <= map_y_min or snake_y >= map_y_max - 8 then
+    if snake_y[1] <= map_y_min or snake_y[1] >= map_y_max - 8 then
         game_over = true
         return
     end
-    if snake_x == fruit_x and snake_y == fruit_y then
+    if snake_x[1] == fruit_x and snake_y[1] == fruit_y then
         score = score + 10
         Update_fruit()
     end
@@ -97,7 +97,7 @@ function Update_fruit()
     while true do
         fruit_x = math.random(0, 256)
         fruit_y = math.random(8, 192)
-        if (fruit_x ~= snake_x or fruit_y ~= snake_y) and
+        if (fruit_x ~= snake_x[1] or fruit_y ~= snake_y[1]) and
         (fruit_x % 8 == 0 and fruit_y % 8 == 0) then
             break
         end
