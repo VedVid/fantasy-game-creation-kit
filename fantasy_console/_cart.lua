d6a86500dd9a77e920c07704ffdb45b0b5e06b31
Tomasz Nowakowski <toalak@gmail.com>
2024-06-05 23:29:55 +0200

    Foundation for drawing other segments of snake than head

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -76,16 +76,23 @@ end
 
 
 function Update_snake()
+    local tmp_x_1 = snake_x[1]
+    local tmp_y_1 = snake_y[1]
+    local tmp_x_2, tmp_y_2
+
     snake_x[1] = snake_x[1] + (x_dir * 8)
     snake_y[1] = snake_y[1] + (y_dir * 8)
+
     if snake_x[1] <= map_x_min or snake_x[1] >= map_x_max - 8 then
         game_over = true
         return
     end
+
     if snake_y[1] <= map_y_min or snake_y[1] >= map_y_max - 8 then
         game_over = true
         return
     end
+
     if snake_x[1] == fruit_x and snake_y[1] == fruit_y then
         score = score + 10
         for i = #snake_x + 1, 2, -1 do
@@ -95,6 +102,15 @@ function Update_snake()
         snake_x[1] = fruit_x
         snake_y[1] = fruit_y
         Update_fruit()
+    end
+
+    for i = 2, #snake_x do
+        tmp_x_2 = snake_x[i]
+        tmp_y_2 = snake_y[i]
+        snake_x[i] = tmp_x_1
+        snake_y[i] = tmp_y_1
+        tmp_x_1 = tmp_x_2
+        tmp_y_1 = tmp_y_2
     end
 end
 
