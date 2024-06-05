a2d925da46a9521a12d379edca1fb130c6417a93
Tomasz Nowakowski <toalak@gmail.com>
2024-06-05 21:50:30 +0200

    Add map boundaries and game over condition

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -7,6 +7,11 @@ local snake_x = 0
 local snake_y = 0
 local fruit_x = 0
 local fruit_y = 0
+local map_x_min = 0
+local map_x_max = 256
+local map_y_min = 8
+local map_y_max = 192
+local game_over = false
 
 function Init()
     x_dir = 1
@@ -33,20 +38,38 @@ function Input()
 end
 
 function Update()
+    if game_over == true then
+        return
+    end
     snake_x = snake_x + (x_dir * 8)
     snake_y = snake_y + (y_dir * 8)
+    if snake_x <= map_x_min or snake_x >= map_x_max - 8 then
+        game_over = true
+    end
+    if snake_y <= map_y_min or snake_y >= map_y_max - 8 then
+        game_over = true
+    end
 end
 
 function Draw()
     Cls()
+    Draw_score_bg()
     Draw_score()
     Draw_fruit()
     Draw_snake()
+    if game_over == true then
+        Write("GAME OVER", 100, 1)
+    end
 end
 
 
 function Draw_score()
     Write("Score: " .. score, 1, 1, White)
+end
+
+
+function Draw_score_bg()
+    Rectfill(0, 0, 256, 8, Cyan)
 end
 
 
