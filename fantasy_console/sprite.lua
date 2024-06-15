7567325f389d0939149b1bcb5c4d9d704b97f505
Tomasz Nowakowski <toalak@gmail.com>
2024-06-16 00:46:59 +0200

    Naive and untested skeleton of: list of sprites defined in game (it should store all sprite slots all the time, even the empty ones), finding correct sprite from the cartdata, setting specific pixel to specific color within a sprite

---
+++ fantasy_console/sprite.lua
@@ -0,0 +1,30 @@
+local cartdata = require "_cartdata"
+local g = require "globals"
+local palette = require "palette"
+
+local sprite = {}
+
+sprite.default_color = g.colors.default_bg_color
+
+function sprite.new_sprite()
+	local new_sprite = {}
+	local new_sprite.colors = {}
+	for y = 1, 8 do
+		local tmp_colors = {}
+		for x = 1, 8 do
+			table.insert(tmp_colors, sprite.default_color)
+		end
+		table.insert(new_sprite.colors, tmp_colors)
+	end
+	return new_sprite()
+end
+
+function sprite.set_pixel_color(sprite_no, pix_x, pix_y, color)
+	-- tables are passed as reference, so it should update, not?
+	local sprite = sprite.get_sprite_from_cartdata(sprite)
+	sprite.colors[x][y] = color
+end
+
+function sprite.get_sprite_from_cartdata(sprite)
+	return cartdata.sprites[sprite]
+end
