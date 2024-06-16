50271ccf114f97a2ededf55ea77483acd3694cbb
Tomasz Nowakowski <toalak@gmail.com>
2024-06-16 12:55:21 +0200

    Tiny fixes to the sprites first implementation

--- fantasy_console/sprite.lua
+++ fantasy_console/sprite.lua
@@ -1,6 +1,5 @@
 local cartdata = require "_cartdata"
 local g = require "globals"
-local palette = require "palette"
 
 local sprite = {}
 
@@ -8,7 +7,7 @@ sprite.default_color = g.colors.default_bg_color
 
 function sprite.new_sprite()
 	local new_sprite = {}
-	local new_sprite.colors = {}
+	new_sprite.colors = {}
 	for y = 1, 8 do
 		local tmp_colors = {}
 		for x = 1, 8 do
@@ -21,8 +20,8 @@ end
 
 function sprite.set_pixel_color(sprite_no, pix_x, pix_y, color)
 	-- tables are passed as reference, so it should update, not?
-	local sprite = sprite.get_sprite_from_cartdata(sprite)
-	sprite.colors[x][y] = color
+	local sprite = sprite.get_sprite_from_cartdata(sprite_no)
+	sprite.colors[pix_x][pix_y] = color
 end
 
 function sprite.get_sprite_from_cartdata(sprite)
