069ef03b99475b9b7dc69e390636c3ea8670a0b9
Tomasz Nowakowski <toalak@gmail.com>
2024-08-21 18:57:54 +0200

    Removed unnecessary variables from windows batch script

--- start_tests.bat
+++ start_tests.bat
@@ -1,7 +1,4 @@
 @echo off
-set filenamestart="testreport"
-set filenameend=".txt"
-set "filenametotal=%filenamestart% %date% %time% %filenameend%"
 cd fantasy_console
 lua5.1.exe api_test.lua -v -o TAP
 lua5.1.exe canvas_test.lua -v -o TAP
