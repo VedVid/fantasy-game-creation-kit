93ba156cb509fedbbe65210d1deef0b1cfbd08cd
Tomasz Nowakowski <toalak@gmail.com>
2024-08-25 22:25:05 +0200

    Positioning of spritesheet finished

--- fantasy_console/sprite_editor.lua
+++ fantasy_console/sprite_editor.lua
@@ -16,15 +16,14 @@ function editor.draw_all_sprites(tab)
 		start = cols * rows * tab
 	end
 
-	local x = 4
-	local y = 4
+	local x = g.sprites.size_w
+	local y = 192 - ((rows + 1) * g.sprites.size_h)
 
-	for i=start, start+(cols*rows) do
+	for i=start, cols*rows do
 		Spr(i, x, y)
 		x = x + g.sprites.size_w
-		if x >= g.screen.size.gamepixels.w - 4 then
-			print(x)
-			x = 4
+		if x >= g.screen.size.gamepixels.w - (1.5 * g.sprites.size_w) then
+			x = g.sprites.size_w
 			y = y + g.sprites.size_h
 		end
 	end
