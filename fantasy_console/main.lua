13ffd4c680fc43f2fa154242973f0da78ab1a6b5
Tomasz Nowakowski <toalak@gmail.com>
2024-08-25 22:55:46 +0200

    Unnecessary argument removed

--- fantasy_console/main.lua
+++ fantasy_console/main.lua
@@ -68,7 +68,7 @@ function love.draw()
         Draw()
     elseif mode == "sprites" then
         love.graphics.clear(g.colors.default_fg_color.rgb01)
-        sprite_editor.draw_all_sprites(3)
+        sprite_editor.draw_all_sprites()
     end
     local current_time = love.timer.getTime()
     if g.next_time <= current_time then
