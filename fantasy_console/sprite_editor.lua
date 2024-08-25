d72690678e07e05621326d579e9aa1c139dbbe17
Tomasz Nowakowski <toalak@gmail.com>
2024-08-25 22:14:41 +0200

    Small fixes to initial spritesheet positions

--- fantasy_console/sprite_editor.lua
+++ fantasy_console/sprite_editor.lua
@@ -19,13 +19,13 @@ function editor.draw_all_sprites(tab)
 	local x = 4
 	local y = 4
 
---	Spr(1, x, y)
 	for i=start, start+(cols*rows) do
 		Spr(i, x, y)
-		x = x + g.sprites.size_h
-		if x > g.screen.size.gamepixels.w then
+		x = x + g.sprites.size_w
+		if x >= g.screen.size.gamepixels.w - 4 then
+			print(x)
 			x = 4
-			y = y + 8
+			y = y + g.sprites.size_h
 		end
 	end
 end
