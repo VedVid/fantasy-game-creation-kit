bc0be819b1933eea37ce705d8b91473a4e57f3fa
Tomasz Nowakowski <toalak@gmail.com>
2024-09-08 23:23:53 +0200

    Improved collision detection for buttons

--- fantasy_console/utils.lua
+++ fantasy_console/utils.lua
@@ -5,4 +5,13 @@ function utils.check_if_string_is_valid_ascii(s)
     return tmp == s
 end
 
+function utils.mouse_box_bound_check(x, x_min, x_max, y, y_min, y_max)
+    return (
+        x_min <= x and
+        x_max >= x and
+        y_min <= y and
+        y_max >= y
+    )
+end
+
 return utils
