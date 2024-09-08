0766b07622edff50c88e7c58c238b9bd6fe7b568
Tomasz Nowakowski <toalak@gmail.com>
2024-09-08 16:46:07 +0200

    No longer necessary profiling removed

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -15,15 +15,11 @@ function Update()
     do end
 end
 
-ProFi = require 'ProFi'
 function Draw()
-    ProFi:start()
     Spr(1, 0, 0)
     Pset(0, 1, Blue)
     Spr(1, 8, 8)
     Pset(0, 2, Blue)
     Spr(1, 16, 16)
     Pset(0, 3, Blue)
-    ProFi:stop()
-    ProFi:writeReport( 'MyProfilingReport.txt' )
 end
