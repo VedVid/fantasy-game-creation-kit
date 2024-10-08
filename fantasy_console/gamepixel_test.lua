8802621e28c5afae4912de1fd4ffef31c03c0e0c
Tomasz Nowakowski <toalak@gmail.com>
2024-10-08 21:47:39 +0200

    Gamepixel tests fixed

--- fantasy_console/gamepixel_test.lua
+++ fantasy_console/gamepixel_test.lua
@@ -13,8 +13,8 @@ TestGamepixel = {}
 function TestGamepixel:test__should_create_gamepixel_with_default_values__when_arguments_are_set_to_nil()
     local pixel = gamepixel.new_gamepixel()
 
-    luaunit.assertEquals(pixel.x, g.screen.gamepixel.w)
-    luaunit.assertEquals(pixel.y, g.screen.gamepixel.h)
+    luaunit.assertEquals(pixel.x, 1)
+    luaunit.assertEquals(pixel.y, 1)
     luaunit.assertEquals(pixel.color, gamepixel.default_color)
 end
 
@@ -22,8 +22,8 @@ end
 function TestGamepixel:test__should_create_gamepixel_with_custom_values__when_arguments_are_provided()
     local pixel = gamepixel.new_gamepixel(12, 28, palette.blue_bold)
 
-    luaunit.assertEquals(pixel.x, 12*g.screen.gamepixel.h)
-    luaunit.assertEquals(pixel.y, 28*g.screen.gamepixel.h)
+    luaunit.assertEquals(pixel.x, 12)
+    luaunit.assertEquals(pixel.y, 28)
     luaunit.assertEquals(pixel.color, palette.blue_bold)
 end
 
