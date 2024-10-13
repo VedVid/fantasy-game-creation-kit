4b75104a5eaf5d3d8ac8caaf846cc7ef8e5f3efe
Tomasz Nowakowski <toalak@gmail.com>
2024-10-13 11:41:46 +0200

    Tests for rect and rectfill added

--- fantasy_console/api_geometry_calculations_test.lua
+++ fantasy_console/api_geometry_calculations_test.lua
@@ -14,6 +14,8 @@ function TestGeometryCalculations.setUp()
 end
 
 
+-- Start of test_pset
+
 function TestGeometryCalculations.test_pset__should_return_correct_coords__when_passed_correct_arguments()
     local expected_result = {{x = 20, y = 25}}
 
@@ -22,6 +24,8 @@ function TestGeometryCalculations.test_pset__should_return_correct_coords__when_
     luaunit.assertEquals(given_result, expected_result)
 end
 
+
+-- Start of test_line
 
 function TestGeometryCalculations.test_line__should_return_correct_coords__when_calculating_horizontal_line()
     local expected_result = {
@@ -84,6 +88,128 @@ function TestGeometryCalculations.test_line__should_return_correct_coords__when_
     }
 
     local given_result = agc.line(5, 5, 10, 12)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+-- Start of test_rect
+
+function TestGeometryCalculations.test_rect__should_return_correct_anchor_point_and_dimensions__when_calculating_square()
+    local expected_result = {
+        lx = 27.5,
+        ly = 27.5,
+        lw = 20,
+        lh = 20
+    }
+
+    local given_result = agc.rect(5, 5, 5, 5)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+function TestGeometryCalculations.test_rect__should_return_correct_anchor_point_and_dimensions__when_calculating_long_rect()
+    local expected_result = {
+        lx = 52.5,
+        ly = 52.5,
+        lw = 95,
+        lh = 20
+    }
+
+    local given_result = agc.rect(10, 10, 20, 5)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+function TestGeometryCalculations.test_rect__should_return_correct_anchor_point_and_dimensions__when_calculating_high_rect()
+    local expected_result = {
+        lx = 7.5,
+        ly = 17.5,
+        lw = 20,
+        lh = 70
+    }
+
+    local given_result = agc.rect(1, 3, 5, 15)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+function TestGeometryCalculations.test_rect__should_return_correct_anchor_point_and_dimensions__when_calculating_tinies_possible_rect()
+    local expected_result = {
+        lx = 27.5,
+        ly = 27.5,
+        lw = 0,
+        lh = 0
+    }
+
+    print("If API function is called with `1` for `w` and `h` parameters,")
+    print("it will not print rectangle at all. It is expected tradeoff.")
+
+    local given_result = agc.rect(5, 5, 1, 1)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+-- Start of test_rectfill
+
+function TestGeometryCalculations.test_rectfill__should_return_correct_anchor_point_and_dimensions__when_calculating_square()
+    local expected_result = {
+        lx = 25,
+        ly = 25,
+        lw = 25,
+        lh = 25
+    }
+
+    local given_result = agc.rectfill(5, 5, 5, 5)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+function TestGeometryCalculations.test_rectfill__should_return_correct_anchor_point_and_dimensions__when_calculating_long_rect()
+    local expected_result = {
+        lx = 50,
+        ly = 50,
+        lw = 100,
+        lh = 25
+    }
+
+    local given_result = agc.rectfill(10, 10, 20, 5)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+function TestGeometryCalculations.test_rectfill__should_return_correct_anchor_point_and_dimensions__when_calculating_high_rect()
+    local expected_result = {
+        lx = 5,
+        ly = 15,
+        lw = 25,
+        lh = 75
+    }
+
+    local given_result = agc.rectfill(1, 3, 5, 15)
+
+    luaunit.assertEquals(given_result, expected_result)
+end
+
+
+function TestGeometryCalculations.test_rectfill__should_return_correct_anchor_point_and_dimensions__when_calculating_tinies_possible_rect()
+    local expected_result = {
+        lx = 25,
+        ly = 25,
+        lw = 5,
+        lh = 5
+    }
+
+    print("Yes, `rectfill` behaviour is a tiny bit inconsistent with")
+    print("`rect` behaviour")
+
+    local given_result = agc.rectfill(5, 5, 1, 1)
 
     luaunit.assertEquals(given_result, expected_result)
 end
