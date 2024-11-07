63dfba9344b11bb9a6ecb62119528ebcb5b3f972
Tomasz Nowakowski <toalak@gmail.com>
2024-11-07 21:28:50 +0100

    Finished elemental sprites

--- fantasy_console/game/actions.lua
+++ fantasy_console/game/actions.lua
@@ -37,21 +37,73 @@ actions.potion.sprite_active = {
     {106, 107, 108}
 }
 
+actions.fire = {}
+actions.fire.sprite_basic = {
+    {52, 53, 54},
+    {82, 83, 84},
+    {112, 113, 114}
+}
+actions.fire.sprite_active = {
+    {49, 50, 51},
+    {79, 80, 81},
+    {109, 110, 111}
+}
+
+actions.water = {}
+actions.water.sprite_basic = {
+    {58, 59, 60},
+    {88, 89, 90},
+    {118, 119, 120}
+}
+actions.water.sprite_active = {
+    {55, 56, 57},
+    {85, 86, 87},
+    {115, 116, 117}
+}
+
+actions.earth = {}
+actions.earth.sprite_basic = {
+    {121, 122, 123},
+    {151, 152, 153},
+    {124, 125, 126}
+}
+actions.earth.sprite_active = {
+    {127, 128, 129},
+    {157, 158, 159},
+    {154, 155, 156}
+}
+
+actions.air = {}
+actions.air.sprite_basic = {
+    {130, 131, 132},
+    {160, 161, 162},
+    {133, 134, 135}
+}
+actions.air.sprite_active = {
+    {136, 137, 138},
+    {166, 167, 168},
+    {163, 164, 165}
+}
 
 actions.all_actions = {
     enemy = {
         actions.attack,
-        actions.wand,
-        actions.potion
+        actions.fire,
+        actions.earth,
+        actions.water,
+        actions.air
     },
     item = {
         "Pick up",
         "Ignore"
+    },
+    free = {
+        actions.potion
     }
 }
 
 
-local icons_start_x = 52 + 16
+local icons_start_x = 52
 local icons_start_y = 100 + (8 * 4)
 local icons_distance_between_x = 32
 
