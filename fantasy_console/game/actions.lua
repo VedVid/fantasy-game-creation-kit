34ab9a82cdf47857ee894295918146ae93871d04
Tomasz Nowakowski <toalak@gmail.com>
2024-11-07 21:38:22 +0100

    Attacks might be empty or filled; empty attack slots will be drawn in browns

--- fantasy_console/game/actions.lua
+++ fantasy_console/game/actions.lua
@@ -2,88 +2,95 @@ local actions = {}
 
 
 actions.attack = {}
-actions.attack.sprite_basic = {
+actions.attack.sprite_empty = {
     {31, 32, 33},
     {61, 62, 63},
     {91, 92, 93}
 }
-actions.attack.sprite_active = {
+actions.attack.sprite_available = {
     {34, 35, 36},
     {64, 65, 66},
     {94, 95, 96}
 }
+actions.attack.amount = 1
 
 actions.wand = {}
-actions.wand.sprite_basic = {
+actions.wand.sprite_empty = {
     {37, 38, 39},
     {67, 68, 69},
     {97, 98, 99}
 }
-actions.wand.sprite_active = {
+actions.wand.sprite_available = {
     {40, 41, 42},
     {70, 71, 72},
     {100, 101, 102}
 }
+actions.wand.amount = 1
 
 actions.potion = {}
-actions.potion.sprite_basic = {
+actions.potion.sprite_empty = {
     {43, 44, 45},
     {73, 74, 75},
     {103, 104, 105}
 }
-actions.potion.sprite_active = {
+actions.potion.sprite_available = {
     {46, 47, 48},
     {76, 77, 78},
     {106, 107, 108}
 }
+actions.potion.amount = 1
 
 actions.fire = {}
-actions.fire.sprite_basic = {
+actions.fire.sprite_empty = {
     {52, 53, 54},
     {82, 83, 84},
     {112, 113, 114}
 }
-actions.fire.sprite_active = {
+actions.fire.sprite_available = {
     {49, 50, 51},
     {79, 80, 81},
     {109, 110, 111}
 }
+actions.fire.amount = 1
 
 actions.water = {}
-actions.water.sprite_basic = {
+actions.water.sprite_empty = {
     {58, 59, 60},
     {88, 89, 90},
     {118, 119, 120}
 }
-actions.water.sprite_active = {
+actions.water.sprite_available = {
     {55, 56, 57},
     {85, 86, 87},
     {115, 116, 117}
 }
+actions.water.amount = 1
 
 actions.earth = {}
-actions.earth.sprite_basic = {
+actions.earth.sprite_empty = {
     {121, 122, 123},
     {151, 152, 153},
     {124, 125, 126}
 }
-actions.earth.sprite_active = {
+actions.earth.sprite_available = {
     {127, 128, 129},
     {157, 158, 159},
     {154, 155, 156}
 }
+actions.earth.amount = 1
 
 actions.air = {}
-actions.air.sprite_basic = {
+actions.air.sprite_empty = {
     {130, 131, 132},
     {160, 161, 162},
     {133, 134, 135}
 }
-actions.air.sprite_active = {
+actions.air.sprite_available = {
     {136, 137, 138},
     {166, 167, 168},
     {163, 164, 165}
 }
+actions.air.amount = 1
 
 actions.all_actions = {
     enemy = {
@@ -116,7 +123,11 @@ function actions.draw_buttons()
         -- always draw wand, with info about amount of consumables available
         -- always draw potion, with info about amount of consumables available
     for _, action in ipairs(actions.all_actions.enemy) do
-        for i, row in ipairs(action.sprite_basic) do
+        local sprite_to_be_drawn = action.sprite_empty
+        if action.amount > 0 then
+            sprite_to_be_drawn = action.sprite_available
+        end
+        for i, row in ipairs(sprite_to_be_drawn) do
             Spr(current_x, current_y + (8 * i), row[1])
             Spr(current_x + 8, current_y + (8 * i), row[2])
             Spr(current_x + 16, current_y + (8 * i), row[3])
