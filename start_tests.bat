1d059587378d33eeb61461d0a1535fc436caa66e
Tomasz Nowakowski <toalak@gmail.com>
2024-12-05 20:39:51 +0100

    Fix test runner

--- start_tests.bat
+++ start_tests.bat
@@ -2,7 +2,6 @@
 cd fantasy_console
 lua5.1.exe api_test.lua -v -o TAP
 lua5.1.exe canvas_test.lua -v -o TAP
-lua5.1.exe gamepixel_test.lua -v -o TAP
 lua5.1.exe palette_test.lua -v -o TAP
 lua5.1.exe sprite_test.lua -v -o TAP
 lua5.1.exe sprite_editor_test.lua -v -o TAP
