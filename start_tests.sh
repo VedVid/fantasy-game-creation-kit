9eef2951e9b089d76d1f8e27cdd25cfae414f977
Tomasz Nowakowski <toalak@gmail.com>
2024-12-30 18:19:39 +0100

    Removed gamepixel_test.lua from test runner.

--- start_tests.sh
+++ start_tests.sh
@@ -1,7 +1,6 @@
 cd fantasy_console
 lua api_test.lua -v -o TAP
 lua canvas_test.lua -v -o TAP
-lua gamepixel_test.lua -v -o TAP
 lua palette_test.lua -v -o TAP
 lua sprite_test.lua -v -o TAP
 lua sprite_editor_test.lua -v -o TAP
